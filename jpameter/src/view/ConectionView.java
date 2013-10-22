5741fbb4e3a564e9f6634b24e32c969da922bd19
bruna.seliga <bruna.seliga@gmail.com>
2013-10-22 03:13:18 +0000

    Commit inicial das primeiras telas.

---
+++ jpameter/src/view/ConectionView.java
@@ -0,0 +1,136 @@
+package view;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+
+import org.jdesktop.xswingx.PromptSupport;
+import org.jdesktop.xswingx.PromptSupport.FocusBehavior;
+
+import com.towel.swing.img.JImagePanel;
+
+import controller.ActionsController;
+import controller.ConectionController;
+
+public class ConectionView {
+	private JFrame windowConection;
+	private JImagePanel paneConection;
+	private JTextField jtextUrl;
+	private JTextField jtextUser;
+	private JPasswordField jpassSenha;
+	private JButton jbConfirmar;
+	private JComboBox cbBD;
+	private JButton jbAvançar;
+	
+	public ConectionView(){
+		createComponents();
+		setListener();
+	}
+	
+	private void createComponents(){
+		
+		jtextUrl = new JTextField(200);
+		jtextUrl.setBounds(40, 20, 300, 30);
+		
+		jtextUser = new JTextField(40);
+		jtextUser.setBounds(40, 70, 300, 30);
+		
+		jpassSenha = new JPasswordField(40);
+		jpassSenha.setBounds(40, 120, 300, 30);
+		
+		jbConfirmar = new JButton("Confimar");
+		jbConfirmar.setBounds(240, 160, 100, 30);
+		
+		String itens[] = {"Selecione o banco...", "Banco 1", "Banco 2"};
+		cbBD = new JComboBox(itens);
+		cbBD.setBounds(40, 240, 300, 30);
+		cbBD.setEnabled(false);
+		
+		jbAvançar = new JButton("Avançar");
+		jbAvançar.setBounds(285, 320, 100, 30);
+		
+		PromptSupport.setPrompt("URL do Banco de Dados", jtextUrl);
+		PromptSupport.setPrompt("Usuário", jtextUser);
+		PromptSupport.setPrompt("Senha", jpassSenha);
+		
+		PromptSupport.setForeground(Color.BLACK, jtextUrl);
+		PromptSupport.setForeground(Color.BLACK, jtextUser);
+		PromptSupport.setForeground(Color.BLACK, jpassSenha);
+		
+		PromptSupport.setFocusBehavior(FocusBehavior.SHOW_PROMPT, jtextUrl);
+		PromptSupport.setFocusBehavior(FocusBehavior.SHOW_PROMPT, jtextUser);
+		PromptSupport.setFocusBehavior(FocusBehavior.SHOW_PROMPT, jpassSenha);
+		
+		try {
+			//paneConection = new JImagePanel("/home/chico/workspace/jpameter/src/res/tweed.png");
+			paneConection = new JImagePanel(getClass().getResource("../res/tweed.png").getPath().toString());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		paneConection.setFillType(JImagePanel.FillType.CENTER);
+		paneConection.setLayout(null);
+		paneConection.setSize(400, 400);
+		paneConection.setLocation(0, 0);
+		paneConection.setBackground(new Color(222, 222, 222, 0));
+		
+		//Adição dos componentes do Jpanel
+		paneConection.add(jtextUrl);
+		paneConection.add(jtextUser);
+		paneConection.add(jpassSenha);
+		paneConection.add(jbConfirmar);
+		paneConection.add(cbBD);
+		paneConection.add(jbAvançar);
+		
+		//Configuracoes do Jframe
+		windowConection = new JFrame("JPA Meter - Configurações Iniciais");
+		windowConection.setSize(400, 400);
+		windowConection.add(paneConection);
+		windowConection.setLayout(null);
+		windowConection.setVisible(true);
+		windowConection.setResizable(false);
+		windowConection.setLocationRelativeTo(null);
+		windowConection.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+	public void setListener(){
+		jbConfirmar.addActionListener(new ActionsController(this));
+	}
+	
+	public String getUser(){
+		return jtextUser.getText();
+	}
+	
+	public String getPass(){
+		return String.valueOf(jpassSenha.getPassword());
+	} 
+	
+	public String getUrl(){
+		return jtextUrl.getText();
+	}
+	
+	public JComboBox getcbBD(){
+		return cbBD;
+	}
+
+	public JFrame getJFrame(){
+		return windowConection;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new ConectionView();
+	}
+
+}
---
+++ jpameter/src/view/JPAMeterView.java
@@ -0,0 +1,131 @@
+package view;
+
+import java.awt.Color;
+
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTabbedPane;
+
+public class JPAMeterView {
+
+	private JFrame windowConection;
+	
+	//Lado esquerdo
+	private JPanel paneOptions;
+	private JPanel jpbPersistence;
+	private JLabel jlTecnlogia;
+	private JLabel jlConexoes;
+	private JLabel jlOperacoes;
+	private JRadioButton jrbHibernate;
+	private JRadioButton jrbOpenJPA;
+	private JRadioButton jrbEclipseLink;
+	private JCheckBox jcAbas;
+	private ButtonGroup bGroup;
+	private JComboBox cbBD;
+	
+	//Lado direito
+	private JLabel jlOperacao;
+	private JTabbedPane jtAbas;
+	private JPanel jp250R;
+	private JPanel jp1000R;
+	private JPanel jpMedia;
+
+	public JPAMeterView() {
+		createComponents();
+	}
+
+	private void createComponents() {
+
+		//Lado esquerdo ======================================================
+		jlTecnlogia = new JLabel("Tecnologias de Persistência");
+		jlTecnlogia.setBounds(10, 20, 300, 30);
+
+		jrbHibernate = new JRadioButton("Hibernate");
+		jrbHibernate.setBackground(Color.WHITE);
+		jrbOpenJPA = new JRadioButton("Open JPA");
+		jrbOpenJPA.setBackground(Color.WHITE);
+		jrbEclipseLink = new JRadioButton("Eclipse Link");
+		jrbEclipseLink.setBackground(Color.WHITE);
+		
+		bGroup = new ButtonGroup();
+		bGroup.add(jrbEclipseLink);
+		bGroup.add(jrbHibernate);
+		bGroup.add(jrbOpenJPA);
+		
+		jpbPersistence = new JPanel();
+		jpbPersistence.setLayout(new BoxLayout(jpbPersistence, BoxLayout.Y_AXIS));
+		jpbPersistence.add(jrbEclipseLink);
+		jpbPersistence.add(jrbHibernate);
+		jpbPersistence.add(jrbOpenJPA);
+		jpbPersistence.setBounds(10, 50, 300, 75);
+		jpbPersistence.setBackground(Color.WHITE);
+		
+		jlConexoes = new JLabel("Conexões Simuladas");
+		jlConexoes.setBounds(10, 150, 300, 10);
+		
+		jlOperacoes = new JLabel("Operações");
+		jlOperacoes.setBounds(10, 220, 300, 10);
+
+		String itens[] = { "Selecione a operação", "Op 1", "Op2 2" };
+		cbBD = new JComboBox(itens);
+		cbBD.setBounds(10, 245, 200, 20);
+		
+		jcAbas = new JCheckBox("Ver em abas");
+		jcAbas.setBounds(10, 300, 200, 20);
+		jcAbas.setBackground(Color.WHITE);
+
+		paneOptions = new JPanel();
+		paneOptions.setLayout(null);
+		paneOptions.setSize(250, 360);
+		paneOptions.setLocation(5, 5);
+		paneOptions.setBackground(Color.WHITE);
+
+		// Adição dos componentes do Jpanel
+		paneOptions.add(jlTecnlogia);
+		paneOptions.add(cbBD);
+		paneOptions.add(jpbPersistence);
+		paneOptions.add(jlConexoes);
+		paneOptions.add(jlOperacoes);
+		paneOptions.add(jcAbas);
+		
+		
+		//Lado direito ======================================================
+		jlOperacao = new JLabel("<html><h3>Operação: </h3></html>");
+		jlOperacao.setBounds(260, 5, 300, 20);
+		
+		jp250R = new JPanel();
+		jp1000R = new JPanel();
+		jpMedia = new JPanel();
+		
+		jtAbas = new JTabbedPane();
+		jtAbas.setBounds(260, 30, 725, 330);
+		jtAbas.add(jp250R, "50/250 registros");
+		jtAbas.add(jp1000R, "500/1000 registros");
+		jtAbas.add(jpMedia, "Media");
+		
+		// Configuracoes do Jframe ======================================================
+		windowConection = new JFrame("JPA Meter");
+		windowConection.setSize(1000, 400);
+		windowConection.add(paneOptions);
+		windowConection.add(jtAbas);
+		windowConection.add(jlOperacao);
+		windowConection.setLayout(null);
+		windowConection.setVisible(true);
+		windowConection.setResizable(false);
+		windowConection.setBackground(Color.WHITE);
+		windowConection.setLocationRelativeTo(null);
+		windowConection.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+	public static void main(String[] args) {
+		new JPAMeterView();
+
+	}
+
+}
