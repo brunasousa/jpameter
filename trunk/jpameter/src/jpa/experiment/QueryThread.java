b28cf07b26fcda2d9e77741625e3d70479302dc2
bruna.seliga <bruna.seliga@7d4b9728-de0c-2901-9890-22a59d1394f6>
2014-03-28 20:16:51 +0000

    Continuação da implmentação das classes referentes ao experimento.

--- trunk/jpameter/src/jpa/experiment/QueryThread.java
+++ trunk/jpameter/src/jpa/experiment/QueryThread.java
@@ -1,20 +1,77 @@
 package jpa.experiment;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
 public class QueryThread extends Thread{
 
-	private String url;
-	private String db;
-	private String user;
-	private String pass;
 	private String queryFile;
-	private int timeExec;
+	private long timeExec;
 	
-	public QueryThread(String url, String db, String pass, String user, String queryFile, int timeExec, String type){
-		
+	private final String TYPE_QUERY[] = new String []{"select", "insert", "update", "delete"};	
+	private final int SELECT = 0;
+	private final int INSERT = 1;
+	private final int UPDATE = 2;
+	private final int DELETE = 3;
+	
+	public QueryThread(String queryFile, long timeExec, String type){
+		this.queryFile = queryFile;
+		this.timeExec = timeExec;
 	}
 	@Override
 	public void run() {
 		
+		long fullTime = 0;
+		long time = 0;
+		
+		while(fullTime < timeExec){
+			time = System.currentTimeMillis();
+		
+		//Ler query do arquivo xml
+		//Marcar inicio de uma nova consulta
+		//Executar consulta
+		//Estruturar dados
+		//Finalizar marcacao de tempo
+		//Gravar tempo de execucao em arquivo
+			time = System.currentTimeMillis() - time;
+			fullTime += time;
+		}
+	}
+	
+	private String chooserQuery(){
+		
+		File experimentFile = new File(queryFile);
+		SAXBuilder sb = new SAXBuilder();  
+		Document d = null;
+		try {
+			d = sb.build(experimentFile);
+		} catch (JDOMException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}  
+		String[] queries;
+
+		Element jpameterTag = d.getRootElement();  
+		Element queriesTag = jpameterTag.getChild("queries");
+		
+		List query = queriesTag.getChildren();
+		Iterator i = query.iterator();
+		
+		while(i.hasNext()){
+			Element type = ((Element)i.next()).getChild("type");
+	
+		}
+		
+		
+		return "";
 	}
 	
 	
