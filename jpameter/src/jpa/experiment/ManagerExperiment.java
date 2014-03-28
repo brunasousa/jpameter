b2cf545208e8592013f3b4de71ac960a074f702f
bruna.seliga <bruna.seliga@7d4b9728-de0c-2901-9890-22a59d1394f6>
2014-03-28 20:16:51 +0000

    Continuação da implmentação das classes referentes ao experimento.

--- jpameter/src/jpa/experiment/ManagerExperiment.java
+++ jpameter/src/jpa/experiment/ManagerExperiment.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
@@ -14,7 +15,7 @@ public class ManagerExperiment {
 	
 	public static void main(String[] args) {
 		
-		File experimentFile = new File(args[1]);
+		File experimentFile = new File("/home/bruna/experiment.xml");
 		SAXBuilder sb = new SAXBuilder();  
 		Document d = null;
 		try {
@@ -27,7 +28,7 @@ public class ManagerExperiment {
 		
 		Element jpameter = d.getRootElement();  
 		
-		//Informacoes do banco de dados 
+		//Informacoes do banco de dados (OBS: esses dados são configurados automaticamente no persistense.xml quando o experimento é compilado juntamente com as bibliotecas)
 		Element dbConfig = jpameter.getChild("db-config");
 		String url = dbConfig.getChild("url").getValue();
 		String db = dbConfig.getChild("database").getValue();
@@ -35,6 +36,11 @@ public class ManagerExperiment {
 		String password = dbConfig.getChild("password").getValue();
 		String queryFile = dbConfig.getChild("query_file").getValue();
 		
+		//Numero de threads
+		Element threads = jpameter.getChild("threads");
+		int nThreads = Integer.parseInt(threads.getValue());
+		
+		//Passos do experimento
 		Element experiment = jpameter.getChild("experiment");
 		List steps = experiment.getChildren();
 		Iterator i = steps.iterator(); 
@@ -43,11 +49,17 @@ public class ManagerExperiment {
 			Element step = (Element) i.next();
 			int time = Integer.parseInt(step.getAttributeValue("time"));
 			Iterator q = step.getChildren().iterator();
+			System.out.println("step");
+			System.out.println(time);
 			while(q.hasNext()){
 				Element query = (Element)q.next();
 				int sazon = Integer.parseInt(query.getValue());
-				double timeExe = time/(sazon/100);
-				QueryThread qt =  new QueryThread(url, db, password, user, queryFile, (int)timeExe*60, query.getName());	
+				long timeExe = TimeUnit.MINUTES.toMillis(time)/(100/sazon);
+				System.out.println("sazon: "+sazon);
+				System.out.println("timeExec: "+timeExe);
+				System.out.println(query.getName());
+
+				QueryThread qt =  new QueryThread(queryFile, timeExe, query.getName());	
 				qt.start();
 			}
 			
