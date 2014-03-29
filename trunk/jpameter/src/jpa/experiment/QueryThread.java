5fec40f179c9992b90feaa784aed43213b272763
bruna.seliga@gmail.com <bruna.seliga@gmail.com@7d4b9728-de0c-2901-9890-22a59d1394f6>
2014-03-29 18:52:57 +0000

    Criação das threads para as execuções das queries no banco de dados, utilizando o padrão Observer;

--- trunk/jpameter/src/jpa/experiment/QueryThread.java
+++ trunk/jpameter/src/jpa/experiment/QueryThread.java
@@ -1,30 +1,40 @@
 package jpa.experiment;
-
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Observable;
+import java.util.Random;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
-public class QueryThread extends Thread{
+public class QueryThread extends Observable implements Runnable{
 
 	private String queryFile;
 	private long timeExec;
-	
+	private boolean executing = true;
+	private String name;
+	private int typeQuery = 0;
 	private final String TYPE_QUERY[] = new String []{"select", "insert", "update", "delete"};	
-	private final int SELECT = 0;
-	private final int INSERT = 1;
-	private final int UPDATE = 2;
-	private final int DELETE = 3;
 	
 	public QueryThread(String queryFile, long timeExec, String type){
 		this.queryFile = queryFile;
 		this.timeExec = timeExec;
+		
+		if(type.toLowerCase().equals(TYPE_QUERY[1]))
+			typeQuery =  1;
+		
+		if(type.toLowerCase().equals(TYPE_QUERY[2]))
+			typeQuery =  2;
+			
+		if(type.toLowerCase().equals(TYPE_QUERY[3]))
+			typeQuery =  3;
 	}
+
 	@Override
 	public void run() {
 		
@@ -33,7 +43,7 @@ public class QueryThread extends Thread{
 		
 		while(fullTime < timeExec){
 			time = System.currentTimeMillis();
-		
+//			System.out.println("Query escolhida: "+chooserQuery());
 		//Ler query do arquivo xml
 		//Marcar inicio de uma nova consulta
 		//Executar consulta
@@ -43,10 +53,12 @@ public class QueryThread extends Thread{
 			time = System.currentTimeMillis() - time;
 			fullTime += time;
 		}
+		
+		setExecuting(false);
 	}
 	
 	private String chooserQuery(){
-		
+		Random r = new Random();
 		File experimentFile = new File(queryFile);
 		SAXBuilder sb = new SAXBuilder();  
 		Document d = null;
@@ -57,7 +69,7 @@ public class QueryThread extends Thread{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}  
-		String[] queries;
+		List<String> queries = new ArrayList<String>();
 
 		Element jpameterTag = d.getRootElement();  
 		Element queriesTag = jpameterTag.getChild("queries");
@@ -66,12 +78,36 @@ public class QueryThread extends Thread{
 		Iterator i = query.iterator();
 		
 		while(i.hasNext()){
+//			System.out.println(((Element)i.next()).getChild("type").getValue());
+//			System.out.println(((Element)i.next()).getChild("sql").getValue());
 			Element type = ((Element)i.next()).getChild("type");
-	
+			if(Integer.parseInt(type.getValue())==typeQuery+1){
+				Element sql = ((Element)i.next()).getChild("sql");
+				queries.add(sql.getValue());
+			}
 		}
-		
-		
-		return "";
+		if(queries.size()>0)
+			return queries.get(r.nextInt(queries.size()));
+		else
+			return null;
+	}
+	
+	public boolean isExecuting() {
+		return executing;
+	}
+
+	public void setExecuting(boolean executing) {
+		this.executing = executing;
+		setChanged();
+		notifyObservers();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 	
 	
