7b48aea490a2b4976e096e0f64f6856bda38055d
bruna.seliga <bruna.seliga@gmail.com>
2014-03-29 18:52:57 +0000

    Criação das threads para as execuções das queries no banco de dados, utilizando o padrão Observer;

--- jpameter/src/jpa/experiment/ManagerExperiment.java
+++ jpameter/src/jpa/experiment/ManagerExperiment.java
@@ -2,20 +2,42 @@ package jpa.experiment;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.concurrent.TimeUnit;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
+import org.jfree.chart.plot.ThermometerPlot;
 
-public class ManagerExperiment {
+public class ManagerExperiment extends Thread implements Observer{
+	private String url;
+	private String db;
+	private String user;
+	private String password;
+	private String queryFile;
+	private int nThreads;
+
+	public static void main(String args[]){
+		ManagerExperiment me = new ManagerExperiment();
+		me.start();
+	}
+	private List<QueryThread> queryTs = new ArrayList<QueryThread>();
 	
-	public static void main(String[] args) {
-		
-		File experimentFile = new File("/home/bruna/experiment.xml");
+	private List steps;
+	
+	public ManagerExperiment(){
+		super();
+		getExperimentInformation();
+	}
+
+	public void getExperimentInformation(){
+		File experimentFile = new File("/home/chico/experiment.xml");
 		SAXBuilder sb = new SAXBuilder();  
 		Document d = null;
 		try {
@@ -30,39 +52,70 @@ public class ManagerExperiment {
 		
 		//Informacoes do banco de dados (OBS: esses dados são configurados automaticamente no persistense.xml quando o experimento é compilado juntamente com as bibliotecas)
 		Element dbConfig = jpameter.getChild("db-config");
-		String url = dbConfig.getChild("url").getValue();
-		String db = dbConfig.getChild("database").getValue();
-		String user = dbConfig.getChild("user").getValue();
-		String password = dbConfig.getChild("password").getValue();
-		String queryFile = dbConfig.getChild("query_file").getValue();
+		url = dbConfig.getChild("url").getValue();
+		db = dbConfig.getChild("database").getValue();
+		user = dbConfig.getChild("user").getValue();
+		password = dbConfig.getChild("password").getValue();
+		queryFile = dbConfig.getChild("query_file").getValue();
 		
 		//Numero de threads
 		Element threads = jpameter.getChild("threads");
-		int nThreads = Integer.parseInt(threads.getValue());
+		nThreads = Integer.parseInt(threads.getValue());
 		
 		//Passos do experimento
 		Element experiment = jpameter.getChild("experiment");
-		List steps = experiment.getChildren();
-		Iterator i = steps.iterator(); 
-		
+		steps = experiment.getChildren();
+	}
+
+	@Override
+	public void run() {
+		Iterator i = steps.iterator();
+	
 		while(i.hasNext()){
 			Element step = (Element) i.next();
 			int time = Integer.parseInt(step.getAttributeValue("time"));
 			Iterator q = step.getChildren().iterator();
 			System.out.println("step");
 			System.out.println(time);
+			
 			while(q.hasNext()){
 				Element query = (Element)q.next();
 				int sazon = Integer.parseInt(query.getValue());
 				long timeExe = TimeUnit.MINUTES.toMillis(time)/(100/sazon);
+				System.out.println("Executando step de "+query.getName());
 				System.out.println("sazon: "+sazon);
 				System.out.println("timeExec: "+timeExe);
 				System.out.println(query.getName());
-
-				QueryThread qt =  new QueryThread(queryFile, timeExe, query.getName());	
-				qt.start();
+				
+				for(int j = 1; j<= nThreads; j++){
+					System.out.println("Criando thread "+j+" de "+query.getName());
+					QueryThread qt =  new QueryThread(queryFile, timeExe, query.getName());
+					qt.setName("thread "+j+" de "+query.getName());
+					queryTs.add(qt);
+					qt.addObserver(this);
+					Thread t = new Thread(qt);
+					t.start();
+				}
+				System.out.println("Suspendendo");
+				this.suspend();
 			}
-			
+		}
+	}
+	
+	@Override
+	public void update(Observable o, Object arg) {
+		boolean allFinished = true;
+		System.out.println("Terminando "+((QueryThread)o).getName());
+		for(QueryThread qt:queryTs){
+			if(qt.isExecuting()){
+				allFinished = false;
+				break;
+			}
+		}
+		if(allFinished){
+			this.resume();
+			System.out.println("Vontando de onde parou");
+			queryTs.removeAll(queryTs);
 		}
 	}
 
