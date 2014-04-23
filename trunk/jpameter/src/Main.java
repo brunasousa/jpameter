13d872aa65a026d1a2812e3ba5c337ee30c76674
bruna.seliga@gmail.com <bruna.seliga@gmail.com@7d4b9728-de0c-2901-9890-22a59d1394f6>
2014-04-23 02:37:42 +0000

    Execução do jar por meio do processBuilder.

--- trunk/jpameter/src/Main.java
+++ trunk/jpameter/src/Main.java
@@ -4,6 +4,9 @@ import view.DBMSConnectionGUI;
 public class Main {
 
 	public static void main(String[] args) {
+//		jpa.compiler.Compiler c = new jpa.compiler.Compiler();
+//		c.executeJar();
+		
 		DBMSConnectionGUI gui = new DBMSConnectionGUI();
 		gui.execute();
 	}
